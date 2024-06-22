0f471a691d674d3e843201d680e1d92d2d1af1ab
Jonathan Kirch <jonathankirch101@gmail.com>
2024-06-22 18:09:53 -0300

    test next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images:{  
+    remotePatterns: [
+          {
+              protocol: 'https',
+              hostname: 'https://snack-house2-0.vercel.app/'
+          }, 
+    ]
+}
+};
 
 export default nextConfig;
