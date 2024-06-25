bf57a064eaa01035b4d64d4f06d17d3e794a9736
Jonathan Kirch <jonathankirch101@gmail.com>
2024-06-25 15:18:44 -0300

    add metatags e icon da página

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,10 +4,54 @@ import './globals.css'
 import Provider from './Provider'
 import { ModalCartProvider } from './context/ModalCartContext'
 import { ToastContainer } from 'react-toastify'
+import Head from 'next/head'
 
 export const metadata: Metadata = {
   title: 'Snack House',
-  description: 'A lancheria do momento',
+  icons: '/img/logo-snackhouse.webp',
+  description: 'Desfrute dos melhores lanches em Canabarro, Teutônia! Nossa lancheria oferece uma variedade de escolhas, como: fritas, xis, cachorro quente e picados. Venha experimentar os lanches mais deliciosos da região!',
+  keywords: [
+    "Lanches em Canabarro",
+    "Lancheria em Canabarro",
+    "Hamburgueria Canabarro",
+    "Melhores lanches em Canabarro",
+    "Sanduíches Canabarro",
+    "Lanches rápidos Canabarro",
+    "Delivery de lanches Canabarro",
+    "Xis Canabarro",
+    "Cachorro quente Canabarro",
+    "Lanches artesanais Canabarro",
+    "Lanchonete Canabarro",
+    "Comida rápida Canabarro",
+    "Lanches especiais Canabarro",
+    "Lanches gourmet Canabarro",
+    "Lanches deliciosos Canabarro",
+    "Melhor lancheria Canabarro",
+    "Promoções de lanches Canabarro",
+    "Lanches frescos Canabarro",
+    "Lanches saudáveis Canabarro",
+    "Lanches com entrega Canabarro",
+    "Lanches em Teutônia",
+    "Lancheria em Teutônia",
+    "Hamburgueria Teutônia",
+    "Melhores lanches em Teutônia",
+    "Sanduíches Teutônia",
+    "Lanches rápidos Teutônia",
+    "Delivery de lanches Teutônia",
+    "Xis Teutônia",
+    "Cachorro quente Teutônia",
+    "Lanches artesanais Teutônia",
+    "Lanchonete Teutônia",
+    "Comida rápida Teutônia",
+    "Lanches especiais Teutônia",
+    "Lanches gourmet Teutônia",
+    "Lanches deliciosos Teutônia",
+    "Melhor lancheria Teutônia",
+    "Promoções de lanches Teutônia",
+    "Lanches frescos Teutônia",
+    "Lanches saudáveis Teutônia",
+  ],
+  robots: "index, follow"
 }
 
 export default function RootLayout({
