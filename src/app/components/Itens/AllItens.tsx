ad35e684cc7502308f5dd38e34fe226ab7fa7a01
Jonathan Kirch <jonathankirch101@gmail.com>
2024-07-01 16:30:47 -0300

    trocado h1 para h2

--- src/app/components/Itens/AllItens.tsx
+++ src/app/components/Itens/AllItens.tsx
@@ -8,23 +8,23 @@ export default function AllItens() {
   return (
     <>
       <div>
-        <h1 className='md:text-3xl text-2xl font-bold uppercase bg-black text-yellow-400 p-2 rounded mb-5 text-center mx-2'>Lanches</h1>
+        <h2 className='md:text-3xl text-2xl font-bold uppercase bg-black text-yellow-400 p-2 rounded mb-5 text-center mx-2'>Lanches</h2>
         <Lanches />
       </div>
       <div>
-        <h1 className='text-3xl font-bold uppercase bg-black text-yellow-400 p-2 rounded mb-5 text-center mx-2'>Bebidas</h1>
+        <h2 className='text-3xl font-bold uppercase bg-black text-yellow-400 p-2 rounded mb-5 text-center mx-2'>Bebidas</h2>
         <Bebidas />
       </div>
       <div>
-        <h1 className='text-3xl font-bold uppercase bg-black text-yellow-400 p-2 rounded mb-5 text-center mx-2'>Combos</h1>
+        <h2 className='text-3xl font-bold uppercase bg-black text-yellow-400 p-2 rounded mb-5 text-center mx-2'>Combos</h2>
         <Combos />
       </div>
       <div>
-        <h1 className='text-3xl font-bold uppercase bg-black text-yellow-400 p-2 rounded mb-5 text-center mx-2'>Doces e Salgados</h1>
+        <h2 className='text-3xl font-bold uppercase bg-black text-yellow-400 p-2 rounded mb-5 text-center mx-2'>Doces e Salgados</h2>
         <DocesSalgados />
       </div>
       <div>
-        <h1 className='text-3xl font-bold uppercase bg-black text-yellow-400 p-2 rounded mb-5 text-center mx-2'>Tortas</h1>
+        <h2 className='text-3xl font-bold uppercase bg-black text-yellow-400 p-2 rounded mb-5 text-center mx-2'>Tortas</h2>
         <Tortas />
       </div>
     </>
