3cea6a52db809f1f2aa63b13f6f8e0aa72b0b421
Jonathan Kirch <jonathankirch101@gmail.com>
2024-07-05 17:20:45 -0300

    fix config next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,7 @@
 import { withSitemap } from 'next-sitemap';
 
-/** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Configurações do Next.js aqui
+  // configurações específicas do Next.js aqui
 };
 
-export default withSitemap({
-  siteUrl: 'https://www.seusite.com',
-  // outras configurações específicas do sitemap, se necessário
-})(nextConfig);
+export default withSitemap(nextConfig);
