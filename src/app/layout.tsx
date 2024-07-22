4ea1cda2da04592dbfe30f4766e9bb3850f9d04c
Jonathan Kirch <jonathankirch101@gmail.com>
2024-07-22 17:21:38 -0300

    removendo metadata do next e add meta tags normais

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,87 +5,6 @@ import Provider from './Provider'
 import { ModalCartProvider } from './context/ModalCartContext'
 import { ToastContainer } from 'react-toastify'
 
-export const metadata: Metadata = {
-  title: 'Snack House | Lancheria em Teutônia - RS',
-  icons: '/favicon.ico',
-  description:
-    'Desfrute dos melhores lanches em Canabarro, Teutônia! Nossa lancheria oferece uma variedade de escolhas, como: fritas, xis, cachorro quente e picados.',
-  keywords: [
-    'snackhouse',
-    'snack house',
-    'snack house lanches',
-    'Snack house teutonia',
-    'Lanches Teutônia',
-    'Lanches Canabarro',
-    'snackhouse lanches',
-    'Snackhouse teutonia',
-    'snackhouse lanches',
-    'snackhouse lanches canabarro',
-    'snackhouse lanches teutonia',
-    'Lanches em Teutônia',
-    'Lancheria em Teutônia',
-    'Lancheria Teutônia',
-    'Hamburgueria Teutônia',
-    'Melhores lanches em Teutônia',
-    'Sanduíches Teutônia',
-    'Lanches rápidos Teutônia',
-    'Delivery de lanches Teutônia',
-    'Xis Teutônia',
-    'Cachorro quente Teutônia',
-    'Lanches artesanais Teutônia',
-    'Lanchonete Teutônia',
-    'Comida rápida Teutônia',
-    'Lanches especiais Teutônia',
-    'Lanches gourmet Teutônia',
-    'Lanches deliciosos Teutônia',
-    'Melhor lancheria Teutônia',
-    'Promoções de lanches Teutônia',
-    'Lanches frescos Teutônia',
-    'Lanches saudáveis Teutônia',
-    'Lanches com entrega Teutônia',
-    'Lanches em Canabarro',
-    'Lancheria em Canabarro',
-    'Hamburgueria Canabarro',
-    'Melhores lanches em Canabarro',
-    'Sanduíches Canabarro',
-    'Lanches rápidos Canabarro',
-    'Delivery de lanches Canabarro',
-    'Xis Canabarro',
-    'Cachorro quente Canabarro',
-    'Lanches artesanais Canabarro',
-    'Lanchonete Canabarro',
-    'Comida rápida Canabarro',
-    'Lanches especiais Canabarro',
-    'Lanches gourmet Canabarro',
-    'Lanches deliciosos Canabarro',
-    'Melhor lancheria Canabarro',
-    'Promoções de lanches Canabarro',
-    'Lanches frescos Canabarro',
-    'Lanches saudáveis Canabarro',
-    'Lanches com entrega Canabarro',
-    'Lanches em Teutônia',
-    'Lancheria em Teutônia',
-    'Hamburgueria Teutônia',
-    'Melhores lanches em Teutônia',
-    'Sanduíches Teutônia',
-    'Lanches rápidos Teutônia',
-    'Delivery de lanches Teutônia',
-    'Xis Teutônia',
-    'Cachorro quente Teutônia',
-    'Lanches artesanais Teutônia',
-    'Lanchonete Teutônia',
-    'Comida rápida Teutônia',
-    'Lanches especiais Teutônia',
-    'Lanches gourmet Teutônia',
-    'Lanches deliciosos Teutônia',
-    'Melhor lancheria Teutônia',
-    'Promoções de lanches Teutônia',
-    'Lanches frescos Teutônia',
-    'Lanches saudáveis Teutônia',
-  ],
-  robots: 'index, follow',
-}
-
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -94,16 +13,22 @@ export default function RootLayout({
   return (
     <html lang="pt-br" className="scroll-smooth">
       <head>
-        <link rel="shortcut icon" href="https://snackhouse.app.br/favicon.ico" type="image/x-icon" />
+        <link rel="icon" href="https://snackhouse.app.br/favicon.ico" />
+        <meta name="title" content="Snack House | Lancheria em Teutônia - RS" />
+        <meta name="description" content="Desfrute dos melhores lanches em Canabarro, Teutônia! Nossa lancheria oferece uma variedade de escolhas, como: fritas, xis, cachorro quente e picados." />
+        <meta name="keywords" content="snackhouse, snack house, snack house lanches, Snack house teutonia, Lanches Teutônia, Lanches Canabarro, snackhouse lanches, Snackhouse teutonia, snackhouse lanches, snackhouse lanches canabarro, snackhouse lanches teutonia, Lanches em Teutônia, Lancheria em Teutônia, Lancheria Teutônia, Hamburgueria Teutônia, Melhores lanches em Teutônia, Sanduíches Teutônia, Lanches rápidos Teutônia, Delivery de lanches Teutônia, Xis Teutônia, Cachorro quente Teutônia, Lanches artesanais Teutônia, Lanchonete Teutônia, Comida rápida Teutônia, Lanches especiais Teutônia, Lanches gourmet Teutônia, Lanches deliciosos Teutônia, Melhor lancheria Teutônia, Promoções de lanches Teutônia, Lanches frescos Teutônia, Lanches saudáveis Teutônia, Lanches com entrega Teutônia, Lanches em Canabarro, Lancheria em Canabarro, Hamburgueria Canabarro, Melhores lanches em Canabarro, Sanduíches Canabarro, Lanches rápidos Canabarro, Delivery de lanches Canabarro, Xis Canabarro, Cachorro quente Canabarro, Lanches artesanais Canabarro, Lanchonete Canabarro, Comida rápida Canabarro, Lanches especiais Canabarro, Lanches gourmet Canabarro, Lanches deliciosos Canabarro, Melhor lancheria Canabarro, Promoções de lanches Canabarro, Lanches frescos Canabarro, Lanches saudáveis Canabarro, Lanches com entrega Canabarro, Lanches em Teutônia, Lancheria em Teutônia, Hamburgueria Teutônia, Melhores lanches em Teutônia, Sanduíches Teutônia, Lanches rápidos Teutônia, Delivery de lanches Teutônia, Xis Teutônia, Cachorro quente Teutônia, Lanches artesanais Teutônia, Lanchonete Teutônia, Comida rápida Teutônia, Lanches especiais Teutônia, Lanches gourmet Teutônia, Lanches deliciosos Teutônia, Melhor lancheria Teutônia, Promoções de lanches Teutônia, Lanches frescos Teutônia, Lanches saudáveis Teutônia" />
+        <meta name="robots" content="index, follow" />
         <link
           rel="apple-touch-icon"
           sizes="180x180"
           href="/img/logo-snackhouse-apple-touch-icon.png"
         />
+
         <meta name="google-site-verification" content="Gm1G62fTQ4AwSTZ694p6yMlYr6S_W6V32LSyEKfjYo0" />
+
         <meta property="og:title" content="Snack House | Lancheria em Teutônia - RS" />
         <meta property="og:description" content="Desfrute dos melhores lanches em Canabarro, Teutônia! Nossa lancheria oferece uma variedade de escolhas, como: fritas, xis, cachorro quente e picados." />
-        <meta property="og:image" content="/favicon.ico" />
+        <meta property="og:image" content="https://snackhouse.app.br/favicon.ico" />
         <meta property="og:url" content="https://snackhouse.app.br/" />
         <meta property="og:type" content="website" />
       </head>
